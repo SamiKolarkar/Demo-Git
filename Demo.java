8e85a8f23dc41656f756d1513a4451f0a0e546b7
SamiKolarkar <kolarkarsami@gmail.com>
2025-12-09 16:49:53 +0530

    Changes done

--- Demo.java
+++ Demo.java
@@ -3,6 +3,5 @@ import java.util.*;
 class Demo {
     public static void main(String[] args) {
         System.out.println(">Hello World...");
-        System.out.println(">Hello World, I am learning Git...");
     }
 }
