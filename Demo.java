3d1775c69bddd028d26abc309bc39b3314314b34
SamiKolarkar <kolarkarsami@gmail.com>
2025-12-09 16:41:14 +0530

    Changes made as printing status of writing code

--- Demo.java
+++ Demo.java
@@ -3,5 +3,6 @@ import java.util.*;
 class Demo {
     public static void main(String[] args) {
         System.out.println(">Hello World...");
+        System.out.println(">Hello World, I am learning Git...");
     }
 }
